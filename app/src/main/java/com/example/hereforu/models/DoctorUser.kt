a8e88d11bc2287c21baf85be11ccd302210f4447
khadija <S4338611@studenti.unige.it>
2022-10-14 09:18:37 +0200

    create a Doctor model

--- app/src/main/java/com/example/hereforu/models/DoctorUser.kt
+++ app/src/main/java/com/example/hereforu/models/DoctorUser.kt
@@ -2,12 +2,13 @@ package com.example.hereforu.models
 
 
 
-/** class DoctorUser {
-    private val uid: String,
-    private val name: String,
-    private val cognome: String,
-    private var email: String,
-    private var profileImagePath: String
-    ) {
-        constructor() : this("","","","","")
-    } **/
+ class DoctorUser (
+     val uid: String,
+     val name: String,
+     val cognome: String,
+     val email: String,
+     val numerOrdine:String,
+     val profileImagePath: String)
+ {
+
+ }
