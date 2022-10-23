c5d9f652ed00dc63a6d5f478235cb79f08e847bd
omocaig90 <3393886@studenti.unige.it>
2022-10-23 13:18:44 +0200

    Prove Profilo

---
+++ app/src/main/java/com/example/hereforu/form/UserProfile.kt
@@ -0,0 +1,12 @@
+package com.example.hereforu.form
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hereforu.R
+
+class UserProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
