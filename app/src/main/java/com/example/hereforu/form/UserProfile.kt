20026ec8f4b45855ca7675b8b7980273a72e07d2
khadijan30 <S4338611@studenti.unige.it>
2022-11-02 22:10:27 +0100

    Update.setOnClickListener not working yet

--- app/src/main/java/com/example/hereforu/form/UserProfile.kt
+++ app/src/main/java/com/example/hereforu/form/UserProfile.kt
@@ -3,10 +3,53 @@ package com.example.hereforu.form
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hereforu.R
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.FirebaseUserMetadata
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_user_profile.*
 
 class UserProfile : AppCompatActivity() {
+
+    //private var currentUser=FirebaseAuth.getInstance().currentUser
+    private lateinit var reference:DatabaseReference;
+    private lateinit var auth:FirebaseAuth;
+    private lateinit var UserId:String;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+
+
+        var fullname:String=""
+
+        val uid = FirebaseAuth.getInstance().currentUser?.uid
+        val db = FirebaseDatabase.getInstance().reference
+        val uidRef = db.child("users").child(uid.toString())
+        val valueEventListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                fullname =
+                    snapshot.child("identificativo").getValue<String>().toString()
+                    NameProfile.setText(fullname)
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d("TAG", databaseError.getMessage())
+            }
+        }
+        uidRef.addListenerForSingleValueEvent(valueEventListener)
     }
-}
+
+
+
+    }
--- app/src/main/java/com/example/hereforu/ui/ProfiloFragment.kt
+++ app/src/main/java/com/example/hereforu/ui/ProfiloFragment.kt
@@ -1,11 +1,24 @@
 package com.example.hereforu.ui
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.hereforu.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
+import kotlinx.android.synthetic.main.activity_user_profile.*
+import kotlinx.android.synthetic.main.activity_user_profile.NameProfile
+import kotlinx.android.synthetic.main.fragment_profilo.*
+import com.squareup.picasso.Picasso
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +34,14 @@ class ProfiloFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    var uid = FirebaseAuth.getInstance().currentUser?.uid
+    val db = FirebaseDatabase.getInstance().reference
+    val uidRef = db.child("users").child(uid.toString())
+    var fullname:String="";
+    var description:String="";
+    var srcImageProfile:String="";
+    var cognome:String=""
+    var email:String=""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +49,47 @@ class ProfiloFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        val valueEventListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                srcImageProfile=snapshot.child("profileImagePath").getValue<String>().toString()
+                Picasso.with(this@ProfiloFragment.context).load(srcImageProfile).into(imageProfile)
+                description=snapshot.child("identificativo").getValue<String>().toString();
+                if(description=="2"){
+                    WHO.setText("User")
+                }
+                else{
+                    WHO.setText("DOCTOR")
+                }
+                email= snapshot.child("email").getValue<String>().toString()
+                myEmail.setText(email)
+                fullname =
+                    snapshot.child("name").getValue<String>().toString()
+                    NameProfile.setText(fullname)
+                    myName.setText(fullname)
+                cognome =
+                    snapshot.child("cognome").getValue<String>().toString()
+                    myCognome.setText(cognome)
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d("TAG", databaseError.getMessage())
+            }
+        }
+        uidRef.addListenerForSingleValueEvent(valueEventListener)
+        // Not Working
+        //Update.setOnClickListener{
+            //if(email!=myEmail.text.toString()){
+                //uidRef.child("email").setValue(myEmail.text.toString())
+            //}
+        //}
     }
+    private fun update(){
+        if(email!=myEmail.text.toString()){
+            uidRef.child("email").setValue(myEmail.text.toString())
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
